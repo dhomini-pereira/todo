a12055226776dacd21aacb9a70e525c1fc19b8c2
dhpe <dhpe@brasilcap.com.br>
2024-11-14 19:27:12 -0300

    feat: adicionada pipeline + definindo variáveis globais

--- src/app/globals.tsx
+++ src/app/globals.tsx
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:3000";
-export const API_URL_WS = "ws://localhost:3000";
+export const API_URL = "http://ec2-52-67-117-181.sa-east-1.compute.amazonaws.com:3000";
+export const API_URL_WS = "ws://ec2-52-67-117-181.sa-east-1.compute.amazonaws.com:3000";
