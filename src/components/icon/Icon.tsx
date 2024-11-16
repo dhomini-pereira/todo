63e9c7a23efbae9911c68b3c72793c8d9ca9c279
dhpe <dhpe@brasilcap.com.br>
2024-11-15 21:24:50 -0300

    feat: adicionado dropdown de notificação de convite

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -16,7 +16,8 @@ interface IProps {
     | "atsign"
     | "check"
     | "info"
-    | "warning";
+    | "warning"
+    | "notification";
   className?: string;
   onClick?: (e: any) => void | Promise<void>;
 }
@@ -268,6 +269,22 @@ export default function Icon({ iconName, className, onClick }: IProps) {
             strokeLinecap="round"
             strokeLinejoin="round"
             d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
+          />
+        </svg>
+      ) : iconName == "notification" ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className={className}
+          onClick={onClick}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
           />
         </svg>
       ) : null}
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,10 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../sidebar/Sidebar";
 import { useNavbar } from "@/context/NavbarContext";
 import Icon from "../icon/Icon";
+import api from "@/services/api.service";
+import { API_URL } from "@/app/globals";
 
 type IUser = {
   createdAt: string;
@@ -18,6 +20,16 @@ type IProps = {
 
 export default function Navbar({ user }: IProps) {
   const { isActive, toggle } = useNavbar();
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [invites, setInvites] = useState([]);
+  useState(() => {
+    (async () => {
+      const inviteList = await api.get(`${API_URL}/invite`);
+      setInvites(inviteList.data);
+    })();
+  });
+
+  const toggleDropdown = () => setDropdownOpen(!isDropdownOpen);
 
   return (
     <div className="bg-[#0A070E] w-screen text-slate-200 h-12 text-lg flex items-center justify-between pl-4 pr-10">
@@ -26,16 +38,41 @@ export default function Navbar({ user }: IProps) {
         <h1>ToDo App</h1>
       </div>
 
-      <div className="w-fit flex items-center gap-3 text-base">
+      <div className="relative w-fit flex items-center gap-3 text-base">
         <h2>{user?.username || "Unknown"}</h2>
         {user?.imageURL ? (
           <img
             src={user?.imageURL}
             alt={user?.username}
-            className="h-9 w-9 object-cover rounded-full"
+            className="h-9 w-9 object-cover rounded-full cursor-pointer"
           />
         ) : (
-          <Icon iconName="profile" className="h-9" />
+          <Icon iconName="profile" className="h-9 cursor-pointer" />
+        )}
+        <Icon
+          iconName="notification"
+          className={`size-6 ${invites.length > 0 ? "cursor-pointer" : ""}`}
+          onClick={() => {
+            if (invites.length) {
+              toggleDropdown();
+            }
+          }}
+        />
+
+        {isDropdownOpen && (
+          <div className="absolute top-12 right-0 bg-[#1a1a1a] text-slate-200 w-96 rounded shadow-lg">
+            <ul>
+              {invites.map((invite: any, index) => (
+                <li
+                  className="py-2 hover:bg-[#333] cursor-pointer"
+                  key={index}
+                >
+                  Você foi convidado(a) para a área de trabalho:
+                  {invite.workarea.name}
+                </li>
+              ))}
+            </ul>
+          </div>
         )}
       </div>
     </div>
